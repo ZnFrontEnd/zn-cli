7a713171b9af371908814b125ef6570c67d4ccf6
songtao <songtao_st08@163.com>
2019-02-27 19:20:15 +0800

    :zap:修改了入口文件

---
+++ bin/cli.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const chalk = require('chalk');
+const ora = require('ora');
+const fse = require('fs-extra');
+const fs = require('fs');
+const path = require('path');
+const symbols = require('log-symbols');
+const program = require('commander');
+const download = require('download-git-repo');
+
+const packageJson = require('../package.json');
+const spawn = require('child_process').spawn;
+const log = console.log;
+
+const _download = (path, name, callback) => {
+  download(
+    path,
+    name,
+    {clone: true},
+    err => {
+      if(err) {
+        log(symbols.error, chalk.red(err));
+        if(spinner) {
+          spinner.fail();
+        }
+      } else {
+        callback && callback();
+      }
+    }
+  )
+}
+
+program
+    .version(packageJson.version)
+    .command('init <type> <name>')
+    .description('init a new <type> project')
+    .action((type, name) => {
+      const root =  path.resolve(name);
+      if(!fs.existsSync(name)) {
+        log(`Creating a new ${type} App in ${chalk.green(root)}`)
+        const spinner = ora('Downloading template...').start();
+        if(type === 'react') {
+          // download react template
+          _download(
+            'https://github.com:ZnFrontEnd/zn-cli#react-template',
+            name,
+            () => {
+              spinner.succeed();
+              // 删除脚手架中的build.js文件
+              spawn('rm', ['-rf', `${name}/build.js`]);
+              log(symbols.success, chalk.green('Download successful!'));
+              log(chalk.yellow(
+                `
+                ===========tips==========\n
+                cd ${name} && npm install\n
+                npm run dev
+                `
+              ))
+            }
+          )
+        } else {
+          // 暂时不支持
+          // download vue template
+          // _download(
+          //   'https://github.com:ZnFrontEnd/zn-cli#vue-template',
+          //   name,
+          //   () => {
+          //     spinner.succeed();
+          //     console.log(symbols.success, chalk.green('Download successful!'));
+          //
+          //   }
+          // )
+        }
+      } else {
+        log(symbols.error, chalk.red(`The ${name} folder already exists!`));
+      }
+    })
+
+program.parse(process.argv);
--- bin/index.js
+++ bin/index.js
@@ -3,72 +3,21 @@
 'use strict';
 
 const chalk = require('chalk');
-const ora = require('ora');
-const fse = require('fs-extra');
-const fs = require('fs');
-const path = require('path');
-const symbols = require('log-symbols');
-const program = require('commander');
-const download = require('download-git-repo');
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
 
-const packageJson = require('../package.json');
-const spawn = require('child_process').spawn;
-const log = console.log;
-
-const _download = (path, name, callback) => {
-  download(
-    path,
-    name,
-    {clone: true},
-    err => {
-      if(err) {
-        log(symbols.error, chalk.red(err));
-        if(spinner) {
-          spinner.fail();
-        }
-      } else {
-        callback && callback();
-      }
-    }
-  )
+if(major < 8) {
+  console.error(
+    chalk.red(
+      'You are running Node ' +
+        currentNodeVersion +
+        '.\n' +
+        'Create React/Vue App requires Node 8 or higher. \n' +
+        'Please update your version of Node.'
+    )
+  );
+  process.exit(1);
 }
 
-program
-    .version(packageJson.version)
-    .command('init <type> <name>')
-    .description('init a new <type> project')
-    .action((type, name) => {
-      const root =  path.resolve(name);
-      if(!fs.existsSync(name)) {
-        log(`Creating a new ${type} App in ${chalk.green(root)}`)
-        const spinner = ora('Downloading template...').start();
-        if(type === 'react') {
-          // download react template
-          _download(
-            'https://github.com:youstde/btzhaocha#nativeshare',
-            name,
-            () => {
-              spinner.succeed();
-              // 删除脚手架中的build.js文件
-              spawn('rm', ['-rf', `${name}/build.js`]);
-              console.log(symbols.success, chalk.green('Download successful!'));
-            }
-          )
-        } else {
-          // download vue template
-          _download(
-            'https://github.com:youstde/btzhaocha#nativeshare',
-            name,
-            () => {
-              spinner.succeed();
-              console.log(symbols.success, chalk.green('Download successful!'));
-
-            }
-          )
-        }
-      } else {
-        log(symbols.error, chalk.red(`The ${name} folder already exists!`));
-      }
-    })
-
-program.parse(process.argv);
+require('./cli');
